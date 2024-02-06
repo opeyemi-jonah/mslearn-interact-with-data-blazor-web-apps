2514870f8cf346db967c5e462ee029c52bce79a2
opeyemi-jonah <gavrieljonah@gmail.com>
2024-02-06 10:00:21 -0700

    Added sql lite to startup

---
+++ Services/PizzaService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        private readonly PizzaStoreContext _dbContext;
+
+        public PizzaService(PizzaStoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Pizza[]> GetPizzasAsync()
+        {
+            // Retrieve pizzas from the database asynchronously
+            return await _dbContext.Pizzas.ToArrayAsync();
+        }
+    }
+}
